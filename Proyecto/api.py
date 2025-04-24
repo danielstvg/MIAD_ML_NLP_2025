c4abd4cdb7836888405cd6a6fc17516eacaa2ce0
Daniel Vargas <74126312+danielstvg@users.noreply.github.com>
2025-04-23 20:51:55 -0500

    nuevo modelo

--- Proyecto/api.py
+++ Proyecto/api.py
@@ -1,45 +1,22 @@
-#!/usr/bin/python
-from flask import Flask
-from flask_restx import Api, Resource, fields
+from flask import Flask, request, jsonify
 import joblib
-from m09_model_deployment import predict_proba
+import pandas as pd
 
 app = Flask(__name__)
 
-api = Api(
-    app, 
-    version='1.0', 
-    title='Phishing Prediction API',
-    description='Phishing Prediction API')
+# Cargar el modelo entrenado
+modelo = joblib.load('modelo_popularidad_rf.pkl')
 
-ns = api.namespace('predict', 
-     description='Phishing Classifier')
-   
-parser = api.parser()
+@app.route('/')
+def home():
+    return "API de Predicción de Popularidad en funcionamiento"
 
-parser.add_argument(
-    'URL', 
-    type=str, 
-    required=True, 
-    help='URL to be analyzed', 
-    location='args')
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame([data])
+    pred = modelo.predict(df)
+    return jsonify({'popularidad_predicha': float(pred[0])})
 
-resource_fields = api.model('Resource', {
-    'result': fields.String,
-})
-
-@ns.route('/')
-class PhishingApi(Resource):
-
-    @api.doc(parser=parser)
-    @api.marshal_with(resource_fields)
-    def get(self):
-        args = parser.parse_args()
-        
-        return {
-         "result": predict_proba(args['URL'])
-        }, 200
-    
-    
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
