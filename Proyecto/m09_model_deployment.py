65534b1e489ec88095d086881ebd3a8b349cb3ab
Daniel Vargas <74126312+danielstvg@users.noreply.github.com>
2025-04-23 21:30:41 -0500

    Update m09_model_deployment.py

--- Proyecto/m09_model_deployment.py
+++ Proyecto/m09_model_deployment.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 from flask import Flask
 from flask_restx import Api, Resource, fields
-import joblib
-from m09_model_deployment import predict_proba
+import pickle
 
 app = Flask(__name__)
 
@@ -14,7 +13,14 @@ api = Api(
 
 ns = api.namespace('predict', 
      description='Phishing Classifier')
-   
+
+with open('modelo_ensemble_xgb.pkl', 'rb') as f:
+    modelo = pickle.load(f)
+
+def predict_proba(url):
+    prediction = modelo.predict([url])  
+    return str(prediction[0])
+
 parser = api.parser()
 
 parser.add_argument(
@@ -39,6 +45,6 @@ class PhishingApi(Resource):
         return {
          "result": predict_proba(args['URL'])
         }, 200
-    
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
